9dbc96117d3f419846d645bc1f3828763a19bfab
EDSON <edsondjouda@gmail.com>
2024-11-23 17:46:55 +0100

    Initialisation git

---
+++ css/typeScript/index.js
@@ -0,0 +1,21 @@
+var ecran = document.querySelector('#ecran');
+var btn = document.querySelectorAll('.btn');
+
+for( item of btn){
+    item.addEventListener('click' , (e) => {
+        btntext = e.target.innerText;
+        if(btntext =='÷'){
+            btntext = '/';
+        }
+        if(btntext == '×' ){
+            btntext = '*';
+        }
+        ecran.value += btntext;
+    });
+}
+
+function backspace(){
+    ecran.value = ecran.value.substr(0, ecran.value.length -1);
+}
+
+/* changement du mode de la page */
